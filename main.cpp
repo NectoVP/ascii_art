617bcbe5af4b43b0a04da4bb3194d20b6e9a42cd
pt_va <pt_va@bk.ru>
2024-06-16 20:03:29 +0300

    triangle does not rotate althoug rotation matrix is being applied

--- main.cpp
+++ main.cpp
@@ -12,15 +12,32 @@ struct Point {
     Point() {
         x = 0;
         y = 0;
+        z = 0;
     }
 
-    Point(double x, double y) {
+    Point(double x, double y, double z) {
         this->x = x;
-        this->y = y;
+        this->y = y * 33 / 28;
+        this->z = z;
+    }
+
+    Point(const Point& other) {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+    }
+
+    const Point& operator=(Point other) {
+        x = other.x;
+        y = other.y;
+        z = other.z;    
+        
+        return *this;
     }
 
     double x;
     double y;
+    double z;
 };
 
 class Renderer {
@@ -76,18 +93,82 @@ private:
     DWORD dwBytesWritten;
 };
 
+Point CalculatePerspective(const Point& old) {
+    Point center(0.5, -0.5, 0);
+
+    float fov = 120;
+    double precomp_tan = std::tan(fov * PI / 180 / 2);
+    
+    //Perspective
+    Point tempPoint(old.x / old.z / precomp_tan, old.y / old.z / precomp_tan, old.z);
+    
+    //Translation towards center
+    tempPoint.x += center.x;
+    tempPoint.y += center.y;
+
+    //Final size corresponding to window size
+    tempPoint.x *= nScreenWidth;
+    tempPoint.y *= nScreenHeight;
+
+    return tempPoint;
+}
+
+Point RotatePoint(const Point& point, double x, double y, double z) {
+    x = x * PI / 180; // phi
+    y = y * PI / 180; // psi
+    z = z * PI / 180; // teta
+
+    Point rotated;
+
+    using namespace std;
+    rotated.x = cos(z) * cos(y) * point.x + ( -cos(x) * sin(y) + sin(x) * sin (z) * cos(y)) * point.y + (sin(x) * sin(y) + cos(x) * sin(z) * cos(y)) * point.z;
+    rotated.y = cos(z) * sin(y) * point.x + (cos(x) * cos(y) + sin(x) * sin (z) * sin(y)) * point.y + ( -sin(x) * cos(y) + cos(x) * sin(z) * sin(y)) * point.z;
+    rotated.z = -sin(z) * point.x + sin(x) * cos(z) * point.y + cos(x) * cos(z) * point.z;
+
+    return rotated;
+}
+
+// pass true to check if line is above, false to check below
+bool CheckUnderOrAboveLine(const Point& first, const Point& second, double x, double y, bool flag) {
+    double koef_k = (second.y - first.y) / (second.x - first.x);
+    double koef_b = second.y - koef_k * second.x;
+
+    bool check = koef_k * x + koef_b >= y;
+
+    if(flag)
+        return check;
+
+    return !check;
+}
+
+bool CheckBoundaries(double x, double y) {
+    Point pointA(-0.5, 0, 0.7);
+    Point pointB(0.5, 0, 0.7);
+    Point pointC(0, 0.5, 0.7);
+
+    Point pA = CalculatePerspective(pointA);
+    Point pB = CalculatePerspective(pointB);
+    Point pC = CalculatePerspective(pointC);
+
+    pA = RotatePoint(pA, 45, 0, 0);
+    pB = RotatePoint(pB, 45, 0, 0);
+    pC = RotatePoint(pC, 45, 0, 0);
+
+    return CheckUnderOrAboveLine(pA, pB, x, y, false) && CheckUnderOrAboveLine(pA, pC, x, y, true) && CheckUnderOrAboveLine(pC, pB, x, y, true);
+}
+
+
 int main() {
     Renderer renderer;
-    
+
     double k = 0;
 
     while(true) {
-
         renderer.Fill();
 
         for(int i = 0; i < nScreenHeight; ++i) {
             for(int j = 0; j < nScreenWidth; ++j) {
-                if(std::pow(i - 5 - std::sin(k) * nScreenHeight / 4, 2) / 28 * 33 + std::pow(j - k, 2) / 66 * 28 <= 50)             
+                if(CheckBoundaries(j, -i))
                     renderer.screen[i * nScreenWidth + j] = '.';
             }
         }
