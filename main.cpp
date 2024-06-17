b86180fb3a3c65648cd1b135dc2b00ccdcac601a
pt_va <pt_va@bk.ru>
2024-06-17 22:09:23 +0300

    I am tired I guess I am done

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <windows.h>
 #include <cmath>
+#include<unistd.h>
 
 const double PI = 3.14159265358;
 
-int nScreenWidth = 80;
-int nScreenHeight = 40;
+const int nScreenWidth = 60;
+const int nScreenHeight = 30;
 
 std::string light_ascii = ".,-~:;!*#$@";
 
@@ -44,8 +45,6 @@ struct Point {
 class Renderer {
 public:
     Renderer() {
-        SetWindow(nScreenWidth, nScreenHeight);
-
         screen = new char[nScreenHeight * nScreenWidth];
         hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
         SetConsoleActiveScreenBuffer(hConsole);
@@ -62,29 +61,11 @@ public:
     }
 
     void Fill() {
-        for(int i = 0; i < nScreenHeight; ++i) {
-            for(int j = 0; j < nScreenWidth; ++j) {
-                screen[i * nScreenWidth + j] = ' ';
-            }
-        }
+        memset(screen, 32, nScreenHeight * nScreenWidth);
     }
 
 private:
-    void SetWindow(int Width, int Height) { 
-        _COORD coord; 
-        coord.X = Width; 
-        coord.Y = Height; 
 
-        _SMALL_RECT Rect; 
-        Rect.Top = 0; 
-        Rect.Left = 0;
-        Rect.Bottom = Height - 1; 
-        Rect.Right = Width - 1; 
-
-        HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleScreenBufferSize(Handle, coord);
-        SetConsoleWindowInfo(Handle, TRUE, &Rect);
-    }
 
 public:
     char* screen;
@@ -119,16 +100,18 @@ Point CalculatePerspective(const Point& old, double k, double n) {
 
     Point rotated = RotatePoint(old, 0, n, k);
 
+    //Magic numbers
     rotated.x += 3;
     rotated.z += 4;
 
-    Point light(3.0, 10.0, 3.0);
+    Point light(1.0, 5.0, 3.0);
 
     double light_level = rotated.x * light.x + rotated.y * light.y + rotated.z * light.z;
 
     //Perspective
     Point tempPoint(rotated.x / (rotated.z + offset) / precomp_tan, rotated.y / (rotated.z + offset) / precomp_tan, (rotated.z + offset));
 
+    //Centering
     tempPoint.y += center.y;
     tempPoint.x += center.x;
 
@@ -140,9 +123,25 @@ Point CalculatePerspective(const Point& old, double k, double n) {
     return tempPoint;
 }
 
-int main() {
-    Renderer renderer;
+    void SetWindow(int Width, int Height) { 
+        _COORD coord; 
+        coord.X = Width; 
+        coord.Y = Height; 
 
+        _SMALL_RECT Rect; 
+        Rect.Top = 0; 
+        Rect.Left = 0;
+        Rect.Bottom = Height - 1; 
+        Rect.Right = Width - 1; 
+
+        HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleScreenBufferSize(Handle, coord);
+        SetConsoleWindowInfo(Handle, TRUE, &Rect);
+    }
+
+int main() {
+    SetWindow(nScreenWidth, nScreenHeight);
+    Renderer renderer;
     double n = 0;
 
     while(true) {
@@ -151,7 +150,7 @@ int main() {
         for(double x = -5; x <= 5; x += 0.1) {
             for(double y = -5; y <= 5; y += 0.1) {
                 if(x  * x  + y  * y <= 25) {
-                    for(int k = 0; k < 360; k+=5) {
+                    for(int k = 0; k < 360; k+=7) {
                         Point temp = CalculatePerspective(Point(x / 15, y / 15, 1), k, n);
                         int j = std::round(temp.x);
                         int i = std::round(temp.y);
@@ -163,7 +162,7 @@ int main() {
 
         if(n >= 360)
             n = 0;
-        n+=1;
+        n+=2;
 
         renderer.Render();
     }
